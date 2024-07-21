d8725515c9f91f70df064ea96adbd3c9f02a614f
Chicodup <140813799+Chicodup@users.noreply.github.com>
2024-07-21 17:22:30 +0300

    fewfe
    fewf

--- minigames/main.py
+++ minigames/main.py
@@ -22,9 +22,13 @@ bg = transform.scale(bg,(MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE))
 cyborg_img = image.load("images/ForestRanger.png")
 player_img = image.load("images/rect.png")
 wall_img = image.load("images/pine_tree_png.png")
+ruins_img = image.load("images/Brown-gray_ruins1.png")
 gold_img = image.load("images/clipart1898966.png")
+spike_img = image.load("images/spikes2.png")
 fake_img = image.load("images/clipart1898966.png")
 exit_img = image.load("images/clipart1898966.png")
+button_img = image.load("images/Levelreoundred-min.png")
+button2_img = transform.scale(image.load("images/Levelgreen-min.png"), (70,60))
 all_sprites = sprite.Group()
 
 #створи 2 спрайти та розмісти їх на сцені
@@ -42,7 +46,8 @@ class Player(Sprite):
     def __init__(self,sprite_img,width,height,x,y):
         super().__init__(sprite_img,width,height,x,y)
         self.hp = 100
-        self.speed = 6
+        self.buttons = 0
+        self.speed = 5
         self.left = self.image
         self.right = transform.flip(self.image,True, False)
     def update(self):
@@ -62,6 +67,9 @@ class Player(Sprite):
         collide_list = sprite.spritecollide(self,walls,False,sprite.collide_mask)
         if len(collide_list) > 0:
             self.rect.x, self.rect.y = old_pos
+        collide_list = sprite.spritecollide(self,ruins,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.rect.x, self.rect.y = old_pos
         enemy_collide = sprite.spritecollide(self,enemys,False,sprite.collide_mask)
         if len(enemy_collide) > 0:
             self.hp -= 100
@@ -71,6 +79,19 @@ class Player(Sprite):
         enemy_collide = sprite.spritecollide(self,poshalkikill,False,sprite.collide_mask)
         if len(enemy_collide) > 0:
             self.hp -= 100
+        fake_collide = sprite.spritecollide(self,spikes,False,sprite.collide_mask)
+        if len(fake_collide) > 0:
+            self.hp -= 100
+        collide_list = sprite.spritecollide(self,buttons,False,sprite.collide_mask)
+        for btn in collide_list:
+            btn.image = button2_img
+            buttons.remove(btn)
+        collide_list = sprite.spritecollide(self,buttons,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.rect.x, self.rect.y = old_pos
+
+            
+    
 
 class Enemy(Sprite):
     def __init__(self,sprite_img,width,height,x,y):
@@ -101,12 +122,22 @@ class Enemy(Sprite):
         if len(collide_list) > 0:
             self.dir = choice(self.dir_list)
             self.rect.x, self.rect.y = old_pos
+        collide_list = sprite.spritecollide(self,spikes,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.dir = choice(self.dir_list)
+            self.rect.x, self.rect.y = old_pos
+
         
 
 player = Player(player_img,50,50,300,300)
 walls = sprite.Group()
+ruins = sprite.Group()
 enemys = sprite.Group()
 fakes = sprite.Group()
+spikes = sprite.Group()
+buttons = sprite.Group()
+
+
 exits = sprite.Group()
 poshalkas = sprite.Group()
 poshalkikill = sprite.Group()
@@ -128,6 +159,8 @@ def load_map(map_file):
             for symwol in line:
                 if symwol == "w":
                     walls.add(Sprite(wall_img, TILESIZE,TILESIZE,x,y))
+                if symwol == "r":
+                    ruins.add(Sprite(ruins_img, TILESIZE,TILESIZE,x,y))
                 if symwol == "p":
                     player.rect.x = x
                     player.rect.y = y
@@ -135,16 +168,20 @@ def load_map(map_file):
                     gold = Sprite(gold_img, 40,40,x,y)
                 if symwol == "f":
                     fakes.add(Sprite(fake_img, 40,40,x,y))
+                if symwol == "s":
+                    spikes.add(Sprite(spike_img, 27,27,x,y))
+                if symwol == "b":
+                    buttons.add(Sprite(button_img, 70,60,x,y))
                 if symwol == "a":
-                    exits = (Sprite(exit_img, 40,40,x,y))
+                    exits.add(Sprite(exit_img, 40,40,x,y))
                 if symwol == "e":
                     enemys.add(Enemy(cyborg_img, TILESIZE,TILESIZE,x,y))
                 x += TILESIZE
             y+=TILESIZE
             x = 0
 
-load_map("map1.txt")
-lvl = 1
+load_map("map4.txt")
+lvl = 4
 
 #оброби подію «клік за кнопкою "Закрити вікно"»
 run = True
@@ -159,15 +196,18 @@ while run:
     window.blit(bg, (0,0))
     if player.hp <= 0:
         finish = True
-    if sprite.collide_mask(player,gold):
-        lvl += 1
-        if lvl <= 4:
-            load_map(f"map{lvl}.txt")
+    
     if lvl==4:
-        if sprite.collide_mask(player,exits):
-            finish = True
-            game_over_text = font1.render("     WIN", True,(0,255,0))
-            
+        collide_list = sprite.spritecollide(player,exits,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            if len(buttons) == 0:
+                finish = True
+                game_over_text = font1.render("     WIN", True,(0,255,0))
+    else:
+        if sprite.collide_mask(player,gold):
+            lvl += 1
+            if lvl <= 4:
+                load_map(f"map{lvl}.txt")
 
     all_sprites.draw(window)
     if not finish:
